9638cba23b657c7a0674988852c2fa4b68b2a5ce
absmaa21 <113515770+absmaa21@users.noreply.github.com>
2025-10-22 21:30:08 +0200

    add dark theme

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,14 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import {CssBaseline} from "@mui/material";
+import {createTheme, CssBaseline, ThemeProvider} from "@mui/material";
 import {BrowserRouter} from "react-router-dom";
+
+const theme = createTheme({
+  palette: {
+    mode: 'dark'
+  }
+})
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <CssBaseline/>
-      <App />
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <CssBaseline/>
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
   </React.StrictMode>
 )
